b8aa451b5ab3324f10b21e78a0797bd2002e8854
Irving78rus <isakov911@mail.ru>
2023-02-07 12:40:26 +0300

    add basename

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement||document.createElement('div')
 );
 root.render(
-  <BrowserRouter >
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
    <Provider store={rootReducer}>
     <App />
     </Provider>
