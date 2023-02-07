beeb89ffa34c7414a954545295c1777fb8746a2b
Irving78rus <isakov911@mail.ru>
2023-02-07 23:49:15 +0300

    Updates

--- src/components/pages/contact/modules/ContactsInformation/WarningImage/WarningImageComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { WarningImage } from './style';
-
-interface WarningImageProps {
-  color: string;
-}
-
-export const WarningImageComponent = ({ color }: WarningImageProps) => {
-  return <WarningImage color={color} />;
-};
--- src/components/pages/contact/modules/ContactsInformation/WarningImage/style.ts
+++
@@ -1,8 +0,0 @@
-import { ReactComponent as WarningIcon } from '../../../../../../assets/icon/warning.svg';
-import styled from 'styled-components';
-
-export const WarningImage = styled(WarningIcon)<{ color: string }>`
-  width: 40px;
-  height: 40px;
-  fill: ${(props) => props.color};
-`;
