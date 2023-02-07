e7f33703d189d6d4cdf6753373ec7ed305322873
Irving78rus <isakov911@mail.ru>
2023-02-07 14:37:08 +0300

    ADD

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement||document.createElement('div')
 );
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter  >
    <Provider store={rootReducer}>
     <App />
     </Provider>
